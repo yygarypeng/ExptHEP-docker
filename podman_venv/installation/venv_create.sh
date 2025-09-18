d771bd6bb173537bf605e3e15b33d5aa2ca341a0
Yuan-Yen <yygarypeng@gapp.nthu.edu.tw>
2025-09-18 22:45:36 +0800

    Fix conda create needing to init first bug.

--- podman_venv/installation/venv_create.sh
+++ podman_venv/installation/venv_create.sh
@@ -1,9 +1,2 @@
-conda create -n py310 python==3.10 -y
-conda activate py310
-
-conda install pip numpy pandas scipy matplotlib h5py pytables -y
-conda install conda-forge::mplhep -y
-conda install conda-forge::root -y
-conda install conda-forge::uproot -y
-
+conda env create --name py310 -f py310.yml
 echo "Successfully created and set up the py310 environment with all packages installed."
