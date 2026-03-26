d9b22775968a6938f09332fd589cfa0391889d63
Yuan-Yen <yygarypeng@gapp.nthu.edu.tw>
2026-03-26 16:10:01 +0800

    Fix naming bugs more all users.

--- local_lxplus/run_loc_lxplus.sh
+++ local_lxplus/run_loc_lxplus.sh
@@ -1,7 +1,7 @@
 # Need to have CVMFS mounted to /cvmfs, and data mounted to /root/data/ and /data/shared/
 docker run --name ${USER}_lxplus_local \
     --gpus all \
-    -v /data/ypeng/:/root/data/ \
+    -v /data/${USER}/:/root/data/ \
     -v /data/share/:/root/share/ \
     -v /cvmfs:/cvmfs/ \
     -it atlas-fastframes:v6.4.0
