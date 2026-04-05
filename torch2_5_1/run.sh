d41a8c511115cca83d2ee2e8903149f6a1d3b42d
Gary <yygarypeng@gapp.nthu.edu.tw>
2026-04-05 15:33:43 +0800

    Update run.sh

--- torch2_5_1/run.sh
+++ torch2_5_1/run.sh
@@ -1,4 +1,4 @@
 # Here the data path is set to `/data/${USER}/`  
 # You need to pull the image first with `docker pull yygarypeng/nthu_exphep_torch:latest` 
 # or build it with the provided Dockerfile, and then run the container with the following command:
-docker run --name torch --gpus all -v /data/${USER}/:/root/data/ -it ehep-torch-2.5.1:latest
+docker run --name ypeng_torch251 --gpus all -v /data/${USER}/:/root/data/ -it ehep-torch-2.5.1:latest
